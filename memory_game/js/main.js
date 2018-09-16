27fbe7fbcd348504b2d55201da5bceb7ab18e4fb
David C <davidjcomer@aol.com>
2018-09-16 16:10:00 +0100

    game.js

---
+++ memory_game/js/main.js
@@ -0,0 +1,19 @@
+var cards = ["queen", "queen", "king", "king"];
+var cardsInPlay = [];
+
+var cardOne = cards[1];
+var cardTWo = cards[3];
+
+cardsInPlay.push("cardOne");
+
+console.log("User flipped queen");
+
+cardsInPlay.push("cardTwo");
+
+console.log("User flipped king");
+
+if (cardsInPlay.length === 2) {
+	if (cardsInPlay[0] === cardsInPlay[2]) 
+ } alert("You found a match!");
+else alert("Sorry, try again");
+
